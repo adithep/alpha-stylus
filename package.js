cf880622e27fb6ff487729963fd103a31e128614
Adithep <badsxx@gmail.com>
2014-08-09 12:56:53 +0700

    now

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Expressive, dynamic, robust CSS',
   version: "0.44.0",
-  git: "https://github.com/sbking/meteor-stylus-latest.git"
+  git: "https://github.com/adithep/alpha-stylus.git"
 });
 
 Package._transitional_registerBuildPlugin({
